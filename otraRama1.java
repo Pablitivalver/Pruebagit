46d06ec9216b631e372acb6e7ac1475ec8bd331f
iluva <iluva@LAPTOP-2OPVVEQK>
2020-11-21 18:13:32 +0100

    cambios

---
+++ otraRama1.java
@@ -0,0 +1,12 @@
+	
+public class otraRama1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int numero = 5;
+		
+		System.out.println("El número es: " + numero + " !!");
+		
+	}
+
+}
