62f00d7e0258a7f7575b3fa5c5b9c8fa8f091b9b
iluva <iluva@LAPTOP-2OPVVEQK>
2020-11-21 18:00:56 +0100

    Versión inicial programa prueba

---
+++ prueba1.java
@@ -0,0 +1,11 @@
+
+public class prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int numero = 5;
+		
+		System.out.println("El número es: " + numero);
+	}
+
+}
